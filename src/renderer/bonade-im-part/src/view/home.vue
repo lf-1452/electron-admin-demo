eaafb67e6da54d760bd6334b80c90b5d1ef3815f
DESKTOP-E15NRMQ\Administrator <miracles_y@163.com>
2020-06-24 18:10:08 +0800

    呵呵

--- src/renderer/bonade-im-part/src/view/home.vue
+++ src/renderer/bonade-im-part/src/view/home.vue
@@ -1,6 +1,14 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: YanQi
+ * @Date: 2020-06-24 18:05:08
+ * @LastEditors: YanQi
+ * @LastEditTime: 2020-06-24 18:09:51
+--> 
 <template>
     <div>
-        <div class="drag">拖动窗口</div>
+        <div class="drag">拖动窗口呵呵</div>
         <div @click="max">最大化</div>
         <div @click="min">最小化1</div>
     </div>
