7e2f3cb085f202b8f7992fa4c808dcd46f90cf23
我是个宝宝 <13020106399@163.com>
2020-06-28 10:05:42 +0800

    styleRules.js 修复错误

--- build/commonJs/styleRules.js
+++ build/commonJs/styleRules.js
@@ -1,27 +1,26 @@
-  const {
-      resolveUnderRootDir
-  } = require('../utils')
+const {
+    resolveUnderRootDir
+} = require('../utils')
 
-  module.exports = [{
-          // 配置less文件
-          test: /\.less$/,
-          include: [resolveUnderRootDir('src/renderer/bonade-im-part')],
-          //   loader: "style-loader!css-loader!less-loader",
-          use: [
-              'style-loader',
-              'css-loader',
-              'less-loader',
-              {
-                  loader: 'style-resources-loader',
-                  options: {
-                      patterns: [resolveUnderRootDir('src/renderer/bonade-im-part/src/assets/css/style.less')]
-                  }
-              }
-          ]
-      },
-      {
-          test: /\.css$/, // 用正则匹配文件名， 这里的意思是以.css结尾的文件用如下插件进行转译
-          exclude: '/node_modules/',
-          loader: 'style-loader!css-loader?sourceMap'
-      }
-  ]
+module.exports = [{
+        // 配置scss文件
+        test: /\.scss$/,
+        include: [resolveUnderRootDir('src/renderer/bonade-im-part')],
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+            {
+                loader: 'style-resources-loader',
+                options: {
+                  patterns: [resolveUnderRootDir('src/renderer/bonade-im-part/src/styles/style.scss')]
+                }
+            }
+        ]
+    },
+    {
+        test: /\.css$/, // 用正则匹配文件名， 这里的意思是以.css结尾的文件用如下插件进行转译
+        exclude: '/node_modules/',
+        loader: 'style-loader!css-loader?sourceMap'
+    }
+] 
