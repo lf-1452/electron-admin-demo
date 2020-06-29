0f09842be8965cad041e9bda747b127905367dc8
我是个宝宝 <13020106399@163.com>
2020-06-29 16:43:42 +0800

    修复eslint配置  'parser': 'babel-eslint',   // 必须指定解析器，否则错误难消

--- build/commonJs/jsRules.js
+++ build/commonJs/jsRules.js
@@ -1,20 +1,20 @@
 module.exports = [
-  {
-    test: /\.vue$/,
-    use: [
-      {
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            sass: 'vue-style-loader!css-loader!sass-loader'
-          }
-        }
-      }
-    ]
-  },
-  {
-    test: /\.ts(x?)$/,
-    use:[{
+    {
+        test: /\.vue$/,
+        use: [
+            {
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        sass: 'vue-style-loader!css-loader!sass-loader'
+                    }
+                }
+            }
+        ]
+    },
+    {
+        test: /\.ts(x?)$/,
+        use:[{
             loader: 'babel-loader',
             options: {
                 cacheDirectory: true
@@ -24,58 +24,26 @@ module.exports = [
             loader: 'ts-loader',
             options: { appendTsSuffixTo: [/\.vue$/] }
         }
-    ],
-    include: /src/,
-    exclude: file => (
-        /node_modules/.test(file) &&
+        ],
+        include: /src/,
+        exclude: file => (
+            /node_modules/.test(file) &&
         !/\.vue\.js/.test(file)
-    )
-},
-  {
-    test: /\.js$/,
-    include: /src/,
-    exclude: file => (
-        /node_modules/.test(file) &&
+        )
+    },
+    {
+        test: /\.js$/,
+        include: /src/,
+        exclude: file => (
+            /node_modules/.test(file) &&
         !/\.vue\.js/.test(file)
-    ),
-    use: [{
-        loader: 'babel-loader',
-        options: {
-            cacheDirectory: true
-        }
-    }, 'eslint-loader'
-    ]
-},
-  // {
-  //   test: /\.(js|vue|ts|tsx|jsx)$/,
-  //   exclude: /node_modules/,
-  //   use:[ 
-  //     {
-  //       loader: 'babel-loader'
-  //     },
-  //     {
-  //       loader: 'eslint-loader' ,
-  //       options: {
-  //         fix: false,
-  //         extensions: ['.js', '.jsx', '.vue', '.ts', '.tsx'],
-  //         cache: false,
-  //         emitWarning: true,
-  //         emitError: false
-  //       }
-  //     }
-  //   ]
-    // use:['babel-loader','eslint-loader']
-    // use:[
-    //   {
-    //     loader: 'eslint-loader',
-    //     enforce: 'pre',
-    //     options: {
-    //       fix: false,
-    //       extensions: ['.js', '.jsx', '.vue', '.ts', '.tsx'],
-    //       cache: false,
-    //       emitWarning: true,
-    //       emitError: false
-    //     }
-    //   }
-    // ]
+        ),
+        use: [{
+            loader: 'babel-loader',
+            options: {
+                cacheDirectory: true
+            }
+        }, 'eslint-loader'
+        ]
+    },
 ];
